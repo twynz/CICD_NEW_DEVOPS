1054a0080352e577dff517910a81bb6b3f20705f
twynz <wenyuforwes@163.com>
2019-08-10 22:49:28 +1200

    still need to specify env when start container

---
+++ auth_jenkins/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export CONSUL_ADDRESS=13.237.125.223
+export USER_DB_ADDRESS=personalwebdb.cj6qmmczghu6.ap-southeast-2.rds.amazonaws.com
+export HOST_ADDRESS=54.252.241.252
+export USER_DB_PWD=
+export USER_DB_USER=twy
